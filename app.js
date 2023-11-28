78d67823b553ea5b446570ff914cc8f6b4d234bc
bagasafrz7 <bagasafrz16@gmail.com>
2023-11-28 20:33:55 +0700

    setup project

---
+++ app.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import apiRouter from './routes/api.js';
+import connection from './connection.js';
+
+const env = dotenv.config().parsed;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRouter);
+
+app.use((req, res) => {
+    res.status(404).json({ message: '404 Not Found' });
+})
+
+// MongoDB Connection
+connection();
+
+app.listen(env.APP_PORT, () => {
+    console.log(`Server started on port ${env.APP_PORT}`);
+})
---
+++ connection.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+const env = dotenv.config().parsed;
+
+const connection = () => {
+    mongoose.connect(env.MONGODB_URI, {
+        dbName: env.MONGODB_NAME
+    })
+    
+    const connection = mongoose.connection
+    connection.on('error', console.error.bind(console, 'Connection Error :'))
+    connection.once('open', () => {
+        console.log(`Connected to MongoDB, DB name : ${env.MONGODB_NAME}`)
+    })
+}
+
+export default connection;
